49f3b138ba2e6cfb8ff50bfba2935512004659a4
kapilraghuwanshi <kapil.raghuwanshi5@gmail.com>
2022-10-07 11:27:04 +0530

    added function definition

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 module.exports = {
+  /**
+   * Generates a random string of given length
+   * @param length Length of random string to be generated
+   * @param case optional boolean field which can be true/fase
+   * @returns Random alpha numeric ascii char sting of given length and case
+   */
   randomPasswordGenerator: function (pwdLength = 8, isUppercase = true) {
     const ascii =
       "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
@@ -7,5 +13,5 @@ module.exports = {
       password += ascii[Math.floor(ascii.length * Math.random())];
     }
     return isUppercase ? password.toUpperCase() : password.toLowerCase();
-  },
+  }
 };
