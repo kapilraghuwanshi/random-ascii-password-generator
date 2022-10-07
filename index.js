51431ef6fb302c0eb94d6dd1bae26423cd2f88df
Kapil Raghuwanshi <Kapil.Raghuwanshi5@gmail.com>
2022-10-07 22:34:00 +0530

    changed module export type

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-module.exports = {
   /**
    * Generates a random string of given length
    * @param length Length of random string to be generated
    * @param case optional boolean field which can be true/fase
    * @returns Random alpha numeric ascii char sting of given length and case
    */
-  randomPasswordGenerator: function (pwdLength = 8, isUppercase = true) {
+module.exports.randomPasswordGenerator = (pwdLength = 8, isUppercase = true) => {
     const ascii =
       "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
     let password = "";
@@ -13,5 +12,4 @@ module.exports = {
       password += ascii[Math.floor(ascii.length * Math.random())];
     }
     return isUppercase ? password.toUpperCase() : password.toLowerCase();
-  }
 };
